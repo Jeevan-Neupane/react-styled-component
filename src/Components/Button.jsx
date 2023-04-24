fa2cfb0ba0a1c1ef4fed13b36c512ad2a241f089
Jeevan Neupane <jeevan.neupane003@gmail.com>
2023-04-24 20:09:26 +0545

    Day 1

---
+++ src/Components/Button.jsx
@@ -0,0 +1,16 @@
+import ButtonStyled from "../Styled Components/Button.Styled"
+
+function Button() {
+  return (
+
+       <div>
+      <ButtonStyled>Button</ButtonStyled>
+      <div/>
+      <br/>
+      <ButtonStyled variant='outline'>Button</ButtonStyled>
+    </div>
+  
+  )
+}
+
+export default Button
---
+++ src/Styled Components/Button.Styled.jsx
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+const ButtonStyled = styled.button`
+  border: 2px solid #4caf50;
+  background-color: ${(props)=>props.variant==='outline'?'#fff':'#4caf50'};
+  color:  ${(props)=>props.variant!=='outline'?'#fff':'#4caf50'};
+
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+
+  cursor: pointer;
+
+  transition: 0.5s all ease-out;
+`;
+
+export default ButtonStyled;
